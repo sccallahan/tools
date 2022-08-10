e66d7239b99a3caf957adb5bd25425003f44cc94
S. Carson Callahan <51168718+sccallahan@users.noreply.github.com>
2022-08-10 10:17:06 -0500

    saves a pheatmap with ggplot-like syntax

---
+++ save_pheatmap_png.R
@@ -0,0 +1,8 @@
+# Save pheatmap images similar to ggsave
+
+save_pheatmap_png <- function(x, filename, width=1200, height=1000, res = 150) {
+  png(filename, width = width, height = height, res = res)
+  grid::grid.newpage()
+  grid::grid.draw(x$gtable)
+  dev.off()
+}
